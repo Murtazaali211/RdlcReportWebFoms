ccfc86d83e645a47e839d9cd51f0ee644f59cd49
Murtaza Ali <ali7murtaza@hotmail.com>
2020-09-03 11:27:33 +0500

    initial push

---
+++ ReportTest/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReportTest._Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+<html xmlns="http://www.w3.org/2000/xhtml">
+    <head>
+        <title>Report</title>
+    </head>
+    <body>
+        <form id="form" runat="server">
+            <div style="margin-bottom:10px; margin-left:10px; margin-top:10px;">
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                <asp:Button ID="btnShow" runat="server" Text="Show Report" OnClick="btnShow_Click"></asp:Button>
+            </div>
+            <div style="margin-bottom:10px; margin-left:10px; margin-top:10px;">
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <rsweb:ReportViewer ID="ReportViewer1" runat="server" Width="100%"></rsweb:ReportViewer>
+            </div>
+        </form>
+    </body>
+</html>
+
---
+++ ReportTest/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.Reporting.WebForms;
+
+namespace ReportTest
+{
+    public partial class _Default : Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=sql7005.site4now.net;Initial Catalog=DB_A429E2_FoodDelivery;User Id=DB_A429E2_FoodDelivery_admin; Password=Admin@123;MultipleActiveResultSets=true;");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+
+            string query = "Select * from Product where Product_Name = '" + txtName.Text +"' ";
+            con.Open();
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    ReportDataSet reportDataSet = new ReportDataSet();
+
+                    reportDataSet.Tables["Product"].Merge(dt);
+                    ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                    ReportViewer1.LocalReport.ReportPath = Server.MapPath("ProductReport.rdlc");
+                    ReportDataSource dataSource = new ReportDataSource("ReportDataSet", reportDataSet.Tables[0]);
+                    ReportViewer1.LocalReport.DataSources.Clear();
+                    ReportViewer1.LocalReport.DataSources.Add(dataSource);
+                }
+            }
+        }
+    }
+}
